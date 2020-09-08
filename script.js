fa47acf851f6584b91a952bdc25e73dfbd11f350
Joel Storck <stjo20zs@student.ju.se>
2020-09-08 21:16:10 +0200

    functions added

--- script.js
+++ script.js
@@ -14,61 +14,68 @@ let tryAgainNode = document.createTextNode("Try Again!");
     succesfulDiv.setAttribute("id", "succesfulDiv");
     succesfulDiv.appendChild(welcomeMessage);
     succesfulDiv.appendChild(signOut);
-    welcomeMessage.innerHTML = "<strong>" + "Hjärtligt välkommen till din sida " + "</strong>" + "<i>" + (name) +"!" + "</i>";
+    welcomeMessage.innerHTML = "<strong>" + "Hello " + "</strong>" + "<i>" + localStorage.getItem("username") + "</i>" + "<strong>" + " my" + " excellency!" + "</strong>";
     signOut.setAttribute("id", "signOut");
     signOut.appendChild(signOutNode);
 
     failedDiv.setAttribute("id", "failedDiv");
     failedDiv.appendChild(notWelcomeMessage);
     failedDiv.appendChild(tryAgain);
-    notWelcomeMessage.innerHTML = "<strong>" + "Fel användarnamn/lösenord, var god försök igen! </strong>";
+    notWelcomeMessage.innerHTML = "<strong>" + "Ooops! Something went wrong, please try again.. </strong>";
     tryAgain.setAttribute("id", "tryAgain");
     tryAgain.appendChild(tryAgainNode);
 
-    // Testar funktioner
-    function showSuccess() {
-        document.body.appendChild(succesfulDiv).style.display = "block";
-        console.log("HEJ HEJ HEJ");
-    };
-    
-    // showSuccess();
-
 let checkLS = localStorage.getItem("username");
-if(checkLS){
-    document.body.appendChild(succesfulDiv).style.display = "block";
-    document.body.appendChild(start).style.display = "none";
-    
-    document.getElementById("signOut").addEventListener("click",function() {
-        localStorage.removeItem("username");
-        localStorage.removeItem("password");  
-        document.body.appendChild(start).style.display = "block";
-        document.body.appendChild(succesfulDiv).style.display = "none";
-        })
-}
-
-document.getElementById("login").addEventListener("click",function() {
-    let nameInput = document.getElementById("user").value;
-    let username = String(nameInput);
-    let passwordInput = document.getElementById("password").value;
-    let password = String(passwordInput);
-
-    if(username == namn && password == lösenord){
-        localStorage.setItem("username", username);
-        localStorage.setItem("password", password);
-        document.body.appendChild(succesfulDiv).style.display = "block";
-        document.body.appendChild(start).style.display = "none";
-            console.log(username);
-            console.log(password);
+    if(checkLS){
+        showSuccess();
     }
     else {
-        document.body.appendChild(start).style.display = "none";
-        document.body.appendChild(failedDiv).style.display = "block";
+    showStart();
+}
+function showStart(){
+    document.body.appendChild(start).style.display = "block";
+    document.body.appendChild(failedDiv).style.display = "none";
+    document.body.appendChild(succesfulDiv).style.display = "none";
+        document.getElementById("login").addEventListener("click",function() {
+            let nameInput = document.getElementById("user").value;
+            let username = String(nameInput);
+            let passwordInput = document.getElementById("password").value;
+            let password = String(passwordInput);
+        
+            if(username == namn && password == lösenord){
+                localStorage.setItem("username", username);
+                localStorage.setItem("password", password);
+                document.body.appendChild(succesfulDiv).style.display = "block";
+                document.body.appendChild(start).style.display = "none";
+                    console.log(username);
+                    console.log(password);
+                    showSuccess();
+            }
+            else {
+               showFailed();
+        }
+    });
+}
+    
+function showSuccess() {
+    document.body.appendChild(succesfulDiv).style.display = "block";
+    document.body.appendChild(failedDiv).style.display = "none";
+    document.body.appendChild(start).style.display = "none";
 
-    
+        document.getElementById("signOut").addEventListener("click",function() {
+            localStorage.removeItem("username");
+            localStorage.removeItem("password");  
+            showStart();
+            })
     }
-    
-    document.getElementById("tryAgain").addEventListener("click",function() {
-        document.body.appendChild(start).style.display = "block";
-        document.body.appendChild(failedDiv).style.display = "none";
-        })
-});
+
+function showFailed(){
+    document.body.appendChild(succesfulDiv).style.display = "none";
+    document.body.appendChild(failedDiv).style.display = "block";
+    document.body.appendChild(start).style.display = "none";
+        
+        document.getElementById("tryAgain").addEventListener("click",function() {
+            showStart();
+            })
+}
+
